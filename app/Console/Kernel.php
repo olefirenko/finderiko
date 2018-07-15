a690db9976873a6c98564c48ba554ad83861957d
olefirenko <dmitriy.olefirenko@gmail.com>
2018-07-15 19:11:36 +0800

    Add cache

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         Commands\AmazonParser::class,
         Commands\SitemapGenerator::class,
+        Commands\ClearPageCache::class,
     ];
 
     /**
