e527f4b317dcf2d7b2d40797605bf6c9d6f2f54c
olefirenko <dmitriy.olefirenko@gmail.com>
2018-07-15 18:49:37 +0800

    GA

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,15 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-122305873-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'UA-122305873-1');
+  </script>
   <base href="{{ url('/') }}" />
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
